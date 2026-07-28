0ca686fc274cd8f560d18fb9f1682c6bc5d63b58
celiabensola8-cell <celia.bensola8@icloud.com>
2026-07-28 20:46:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-    script.js
+const tg = window.Telegram.WebApp;
+
+tg.ready();
+tg.expand();
+
+const welcome = document.getElementById("welcome");
+
+if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
+    const user = tg.initDataUnsafe.user;
+    welcome.innerHTML = `👋 Bonjour <b>${user.first_name}</b><br>Bienvenue dans votre studio de création graphique.`;
+}
+
+document.querySelectorAll(".btn").forEach(button => {
+    button.addEventListener("click", () => {
+        tg.HapticFeedback.impactOccurred("light");
+    });
+});
