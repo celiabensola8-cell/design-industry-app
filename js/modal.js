3eedd0f0691bbdf4edb8cd19f30d7a6d363e7987
celiabensola8-cell <celia.bensola8@icloud.com>
2026-07-29 18:55:10 +0200

    Create modal.js

---
+++ js/modal.js
@@ -0,0 +1,7 @@
+function ouvrirModal() {
+    document.getElementById("orderModal").classList.remove("hidden");
+}
+
+function fermerModal() {
+    document.getElementById("orderModal").classList.add("hidden");
+}
